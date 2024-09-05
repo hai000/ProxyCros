60494c4821416f7559081229b9c4aa5b24b473c4
HaiGH-Space <nthai0932169055@gmail.com>
2024-09-05 17:46:10 +0700

    update

--- proxy.js
+++ proxy.js
@@ -5,18 +5,21 @@ const path = require('path');
 
 const app = express();
 
-app.get('/proxy', async (req, res) => {
+app.get('/tots', async (req, res) => {
     const url = req.query.url;
     if (!url) {
         return res.status(400).send('Missing URL parameter');
     }
 
     try {
-        // const response = await axios.get(url, { responseType: 'arraybuffer' });
-        const filePath = path.join(__dirname, 'index.m3u8');
-        // Trả về file
+        const response = await axios.get(url, { responseType: 'arraybuffer' });
+        const filePath = path.join(__dirname, 'index.ts');
+        fs.writeFileSync(filePath, response.data);
+
+
         res.download(filePath, (err) => {
             // Xóa file tạm sau khi gửi
+            fs.unlinkSync(filePath);
             if (err) {
                 console.error(err);
                 return res.status(500).send('Error downloading file');
