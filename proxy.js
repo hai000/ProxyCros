914f6ce2564461b0352922f3c01669a3ae80796a
HaiGH-Space <nthai0932169055@gmail.com>
2024-09-05 20:41:40 +0700

    update

--- proxy.js
+++ proxy.js
@@ -35,14 +35,58 @@ app.get('/tots', async (req, res) => {
 });
 app.get('/index.m3u8',async (req, res) => {
     const url = req.query.url;
+    if (!url) {
+        return res.status(400).send('Missing URL parameter');
+    }
     const filePath = path.join(__dirname, 'index.m3u8');
-    res.download(filePath, (err) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Error downloading file');
-        }
-    });
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    try {
+        await fs.writeFile(filePath, Buffer.from(response.data),err => {
+            if (err){
+                 console.log(
+                     "lõi"
+                 )
+            }else {
+                console.log("Dã đè file")
+            }
+        });
+
+        await modifyFile(filePath,res);
+    } catch (err) {
+        console.error(`Có lỗi xảy ra: ${err.message}`);
+    }
 })
+function modifyFile(filePath,res) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Không thể đọc file: ${err.message}`);
+            return;
+        }
+
+        const lines = data.split('\n'); // Tách file thành các dòng
+        for (let i = 0; i < lines.length; i++) {
+            if (lines[i].includes('#EXTINF:') && i + 1 < lines.length) {
+                lines[i + 1] = 'https://proxycros.onrender.com/tots?url=' + lines[i + 1] ;
+                i++;
+            }
+        }
+
+        // Ghi lại nội dung đã sửa đổi vào file
+        fs.writeFile(filePath, lines.join('\n'), (err) => {
+            if (err) {
+                console.error(`Không thể ghi vào file: ${err.message}`);
+            } else {
+                console.log(`Đã thêm 'ok' vào dòng kế tiếp sau #EXTINF.`);
+                res.download(filePath, (err) => {
+                    if (err) {
+                        console.error(err);
+                        return res.status(500).send('Error downloading file');
+                    }
+                });
+            }
+        });
+    });
+}
 
 const PORT = 443;
 app.listen(PORT, () => {
